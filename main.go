0e42450d68278fbeb6b9aea05bfd67e2ee032e55
I Gusti Agung Anom Semara Putra <agoenkanom1305@gmail.com>
2023-03-13 13:49:08 +0800

    loop and nested  if else

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ import (
 
 func main() {
 
-fmt.Println("kosongan") 
-
-
-
+for i :=0; i<5; i++{
+	fmt.Println("Nilai i = ", i)
+	if i == 4{
+		for j:=0; j<11;j++{
+			
+			if j == 5{
+				for index, value := range "САШАРВО"{
+					fmt.Printf("character %U '%c' starts at byte position %d\n", value,value,index)
+				}
+			} else {
+				fmt.Println("Nilai j = ", j)
+			}
+		}
+	}
+}
 }
